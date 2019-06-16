d73acb30b865b8cd4dc1bf82f019d92919c24822
Denis Denisenko <denis.munch@gmail.com>
2019-06-16 14:24:35 +0700

    Making BERT tokenizer creation lazy.

--- modules/main.py
+++ modules/main.py
@@ -8,10 +8,12 @@ import pandas as pd
 import numpy as np
 import benchmark
 
-bertTokenizer = create_tokenizer(BERT_DIR)
+bertTokenizer = None
 
 @preprocessing.dataset_preprocessor
 def text_to_bert_input(input):
+    global bertTokenizer
+    bertTokenizer = create_tokenizer(BERT_DIR)
     bInput = prepare_input(input, BERT_MAX_SEQ_LENGTH, bertTokenizer, False)
     if bInput.attn_mask is not None:
         return [x[0] for x in [bInput.input_ids, bInput.input_type_ids, bInput.token_pos, bInput.attn_mask]]
@@ -25,27 +27,27 @@ def binarize_target(inp:PredictionItem):
     return inp
 
 class PDDataSet(datasets.DataSet):
-    
-    def __init__(self,df,targetColumn:str,featureColumn,idColumn:str=None,sep=","): 
+
+    def __init__(self,df,targetColumn:str,featureColumn,idColumn:str=None,sep=","):
         self.df=df
         self.feature=self.df[featureColumn].values
-        
+
         if (targetColumn in self.df.columns):
             self._target=self.df[targetColumn].values
         else:
             self._target=np.zeros((len(self),1))
-        if idColumn is not None:    
-            self.ids=self.df[idColumn].values        
+        if idColumn is not None:
+            self.ids=self.df[idColumn].values
         else:
-            self.ids=list(range(len(self.df)))   
-        pass    
-    
+            self.ids=list(range(len(self.df)))
+        pass
+
     def __len__(self):
         return round(len(self.df))
-        
+
     def __getitem__(self, item)->datasets.PredictionItem:
         return PredictionItem(self.ids[item],self.feature[item],np.array([self._target[item]]))
-    
+
     def get_target(self,item):
         return np.array([self._target[item]])
 
@@ -59,11 +61,11 @@ def get_train():
     if _data is None:
         _data=pd.read_csv(get_current_project_path()+"/data/train.csv")
         _ds=PDDataSet(_data, "target","comment_text","id")
-    return _ds    
+    return _ds
 
-@tasks.task        
-def measure(m:model.ConnectedModel,d:datasets.DataSet):    
-    newD=pd.DataFrame(_data.iloc[d.indexes])    
+@tasks.task
+def measure(m:model.ConnectedModel,d:datasets.DataSet):
+    newD=pd.DataFrame(_data.iloc[d.indexes])
     preds=m.predictions(d)
     newD["model"]=pd.Series(np.array(preds.predictions).reshape(-1),index=newD.index)
-    return benchmark.validate(newD)   
+    return benchmark.validate(newD)
